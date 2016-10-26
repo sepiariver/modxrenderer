c895f86a5c2d2c35a1a103d6e6b5f1c148b04782
sepiariver <info@sepiariver.com>
2016-10-26 13:03:29 -0700

    first pass filter

---
+++ src/MODXFilter.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Modifies the output of a MODXTag object.
+ * Can be extended to add more filtering functions.
+ *
+ */
+
+namespace SepiaRiver;
+
+class MODXFilter
+{
+    /**
+     * The input content to filter. Modified in place.
+     *
+     * @var string
+     */
+    protected $input = '';
+    /**
+     * The current condition state
+     *
+     * @var string
+     */
+    protected $condition = null;
+    /**
+     * @constructor
+     */
+    public function __construct()
+    {
+
+    }
+    /**
+     * Set $condition. Can only be performed internally.
+     */
+    protected function setCondition($state) {
+        if ($state === null){
+            $this->condition = null;
+        } else {
+            $this->condition = boolval($state);
+        }
+    }
+    /**
+     * Get $condition
+     */
+    public function getCondition() {
+        return $this->condition;
+    }
+    /**
+     * Prepare filter arguments
+     *
+     * @return array $result
+     */
+    protected function getArgs($args) {
+        $result = [];
+        if (is_string($args)) {
+            $decoded = json_decode($args, true);
+            if (is_array($decoded)) {
+                $result = $decoded;
+            } else {
+                $result['value'] = $args;
+            }
+        }
+        return $result;
+    }
+    /**
+     * "is" filter conditional
+     * conditional filters don't modify the input, only evaluate it and cache results
+     */
+    public function is($input, $args) {
+        $args = $this->getArgs($args);
+        if (isset($args['value'])) {
+            $this->setCondition(($input == $args['value']));
+        }
+    }
+    /**
+     * "isStrictly" filter conditional
+     * conditional filters don't modify the input, only evaluate it and cache results
+     */
+    public function isStrictly($input, $args) {
+        $args = $this->getArgs($args);
+        if (isset($args['value'])) {
+            $this->setCondition(($input === $args['value']));
+        }
+    }
+    /**
+     * "isNot" filter conditional
+     * conditional filters don't modify the input, only evaluate it and cache results
+     */
+    public function isNot($input, $args) {
+        $args = $this->getArgs($args);
+        if (isset($args['value'])) {
+            $this->setCondition(($input != $args['value']));
+        }
+    }
+    /**
+     * "isNotStrictly" filter conditional
+     * conditional filters don't modify the input, only evaluate it and cache results
+     */
+    public function isNotStrictly($input, $args) {
+        $args = $this->getArgs($args);
+        if (isset($args['value'])) {
+            $this->setCondition(($input !== $args['value']));
+        }
+    }
+    /**
+     * "then" filter
+     * result depends on $condition. filters modify input in place.
+     */
+    public function then(&$input, $args) {
+        $args = $this->getArgs($args);
+        if (isset($args['value']) && ($this->getCondition() === true)) {
+            $input = $args['value'];
+            $this->setCondition(null);
+        }
+        // otherwise nothing happens
+    }
+    /**
+     * "else" filter
+     * result depends on $condition. filters modify input in place.
+     */
+    public function else(&$input, $args) {
+        $args = $this->getArgs($args);
+        if (isset($args['value']) && ($this->getCondition() === false)) {
+            $input = $args['value'];
+            $this->setCondition(null);
+        }
+        // otherwise nothing happens
+    }
+
+}
